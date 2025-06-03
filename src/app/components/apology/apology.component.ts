1bf2bb6cf33093ff8b91daf8eb1ff35c6b9c9524
Gean Luiz <geanluiz1@gmail.com>
2025-06-02 22:06:48 -0300

    Created apology component

---
+++ src/app/components/apology/apology.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apology',
+  imports: [],
+  templateUrl: './apology.component.html',
+  styleUrl: './apology.component.css'
+})
+export class ApologyComponent {
+
+}
